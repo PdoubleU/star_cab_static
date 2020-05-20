31af48550e21c610af30f9725696bfcaca892195
PdoubleU <piotrwitasik1988@gmail.com>
2020-05-20 23:15:04 +0200

    1. add variables in action_page.php 2. extend functions of action_page.php: open order.txt, save there data from form, close order.txt

--- dist/php/action_page.php
+++ dist/php/action_page.php
@@ -6,6 +6,30 @@
 <body>
     <h1>The visitor wrote to you:</h1>
     <h2>Message</h2>
-    <?php echo'<p>Wiadomość wysłana!</p>' ?>
+    <?php
+
+	$fname = $_POST['fname'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+	$document_root = $_SERVER['DOCUMENT_ROOT'];
+	$output = $fname. "\t".$email. "\t".$message. "\n";
+	$wp = fopen("$document_root/./order.txt", 'ab');
+
+	if(!$wp) {
+		echo "<p><strong>Nie znajduje pliku!</strong></p>";
+		exit;
+	} else {
+		echo "<p><strong>Plik istnieje</strong></p>";
+	}
+	flock($wp, LOCK_EX);
+	fwrite($wp, $output);
+	fclose($wp);
+
+	echo
+		'<p>Wiadomość wysłana!</p>'
+		.date('H:i, jS F'). '<br/>'
+		.htmlspecialchars($output). '<br/>'
+
+	?>
 </body>
 </html>

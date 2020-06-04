d11102b4451b01a06097ea52df5b24609b5d3845
PdoubleU <piotrwitasik1988@gmail.com>
2020-06-04 22:48:21 +0200

    1.support for ie 11 - add margin auto for contact_form - ie 11 does not support grid!

--- dist/php/content.php
+++ dist/php/content.php
@@ -197,13 +197,13 @@
       <h2 class="contact_form_header">Skontaktuj się z nami</h2>
       <form class="contact_form" action="./php/action_page.php" method="post">
         <label for="fname">First name:</label>
-        <input type="text" id="fname" name="fname"><br>
+        <input type="text" id="fname" name="fname">
         <label for="email">E-mail adress:</label>
-        <input type="email" id="email" name="email"><br>
+        <input type="email" id="email" name="email">
         <label for="confirm_email">Confirm email:</label>
-        <input type="email" id="confirm_email" name="confirm_email"><br>
+        <input type="email" id="confirm_email" name="confirm_email">
         <label for="message">Your message:</label>
-        <textarea type="text" id="message" name="message"></textarea><br>
+        <textarea type="text" id="message" name="message"></textarea>
         <input type="submit" value="Wyślij" class="sub_btn" id="post-data-btn">
       </form>
     </span>

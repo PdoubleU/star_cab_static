6d07d2eb6fad135c55290aefebd15cb8e02b3e94
PdoubleU <piotrwitasik1988@gmail.com>
2020-06-11 01:37:58 +0200

    1. delete src and dist with polish language ver

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars */
-import {handleClick} from "./js-components/burger-button";
-import {changeContent} from "./js-components/change-content";
-import {scroll, scrollTop} from "./js-components/scroll_button_mobile";
-import {slideIn} from "./js-components/onload_head_text";
-import {sentData} from "./js-components/submit_form";
-import {closeModal} from "./js-components/close_modal";
-import {checkEmail} from "./js-components/check_email";

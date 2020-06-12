0a2d8d564a9d3880e41c344e5881a072713f44eb
PdoubleU <piotrwitasik1988@gmail.com>
2020-06-12 03:47:20 +0200

    1. change home.php - wrong id names, unable to load content

--- dist-en/php/home.php
+++ dist-en/php/home.php
@@ -5,7 +5,7 @@
     <title></title>
   </head>
   <body>
-  <article id="about_us" class="column">
+  <article id="home" class="column">
         <h2>Who are we...</h2>
             <p>
             We have been on the market over 15 years.
@@ -13,13 +13,13 @@
             You can trust us.
             </p>
     </article>
-    <article id="about_us"  class="column">
+    <article id="home"  class="column">
         <h2>Where do we go...</h2>
             <p>
             Whit us you can reach almost each place in Europe.
             </p>
     </article>
-    <article id="about_us"  class="column">
+    <article id="home"  class="column">
         <h2>When do we drive...</h2>
             <p>
             We are available 24/7, holidays and weekends include.

30f7e9a11d04efc18e2b9d9d1d9f4c77284a025c
PdoubleU <piotrwitasik1988@gmail.com>
2020-06-16 11:39:03 +0200

    1. change letter mistake in text

--- dist-en/php/tourist_services.php
+++ dist-en/php/tourist_services.php
@@ -9,7 +9,7 @@
     <article id="tourist_services" class="column">
       <h2>Our trips</h2>
         <p>
-          We orginize tours in Wrocław and sourroundings.
+          We organize tours in Wrocław and sourroundings.
           We can take you to most beautiful European cities: Prague, Drezden or Kraków.
           If you think about a specific place to visit - let us know - we will take you there.
           For more information just go to our other website:

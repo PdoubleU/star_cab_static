260cf4d058213e66c3d309f00c950e5b46a844c9
PdoubleU <piotrwitasik1988@gmail.com>
2020-06-19 08:03:31 +0200

    1. delete check-email.js - it is no smart to check email two times (focus out and submit!) 2. change if statements for submit - make it clear and simplest 3. change on submit - inject css style into element with wrong data after testing it

--- src/js/js-components/submit_form.js
+++ src/js/js-components/submit_form.js
@@ -7,37 +7,40 @@ export function sentData(e) {
     e.preventDefault();
     var data = $('.contact_form').serialize();
     const $FNAME = $('#fname').val();
-    let $email_one = $('#email').val();
-    let $email_two = $('#confirm_email').val();
-    let $msg = $('#message').val();
-    let $reg = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$/;
+    const $EMAIL_ONE = $('#email').val();
+    const $EMAIL_TWO = $('#confirm_email').val();
+    const $MSG = $('#message').val();
+    const $REG = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$/;
 
-    if ($reg.test($email_two) && $email_one === $email_two) {
-        if ($msg !== '') {
-                $.post('./php/action_page.php', data)
-                .done(function() {
-                    showModal(`${$FNAME}, <br/> formularz został wysłany. <br/><br/>
-                                Skontaktujemy się z tobą  w ciągu 24 godzin. <br/><br/>
-                                Dziękujęmy!`)})
-                .fail(function(){
-                    showModal(`${$FNAME}, <br/> coś poszło nie tak. <br/><br/>
-                                Spróbuj ponownie lub napisz bezpośrednio na nasz adres email: <br/>
-                                info@starcab.wroclaw.pl`)})
-                $('#fname').val('');
-                $('#email').val('');
-                $('#confirm_email').val('');
-                $('#message').val('');
-                return;
-        }
-        else {
-            return showModal(`Nic nie wpisałeś w polu wiadomość!`);
-        }
+    if ($FNAME === '') {
+        $('#fname').css({'border-color': 'red', 'border-style': 'solid'});
+        return;
     }
-    else {
-        return showModal(`Podany adres e-mail jest nieprawidłowy! <br /> <br />
-                            Spróbuj jeszcze raz`);
+    else if ($MSG === '') {
+        $('#message').css({'border-color': 'red', 'border-style': 'solid'});
+        return;
+    }
+    else if ($EMAIL_ONE !== $EMAIL_TWO || $EMAIL_ONE === '') {
+        $('#email').css({'border-color': 'red', 'border-style': 'solid'});
+        $('#confirm_email').css({'border-color': 'red', 'border-style': 'solid'});
+        return;
+    }
+    else if ($REG.test($EMAIL_TWO)) {
+        $.post('./php/action_page.php', data)
+        .done(function() {
+            showModal(`${$FNAME}, <br/> formularz został wysłany. <br/><br/>
+                Skontaktujemy się z tobą  w ciągu 24 godzin. <br/><br/>
+                Dziękujęmy!`);
+                $FNAME.val('');
+                $EMAIL_ONE.val('');
+                $EMAIL_TWO.val('');
+                $MSG.val('');})
+        .fail(function(){
+            showModal(`${$FNAME}, <br/> coś poszło nie tak. <br/><br/>
+                Spróbuj ponownie lub napisz bezpośrednio na nasz adres email: <br/>
+                info@starcab.wroclaw.pl`)})
+            return;
     }
 };
 
 $SUBMIT.submit(sentData);
-

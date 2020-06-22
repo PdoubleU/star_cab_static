1a7b51df5d018b26fc10804bad2b77367058d63d
PdoubleU <piotrwitasik1988@gmail.com>
2020-06-22 14:21:24 +0200

    change clearing input option in submit_form.js

--- src-en/js/js-components/submit_form.js
+++ src-en/js/js-components/submit_form.js
@@ -6,37 +6,37 @@ const $SUBMIT = $('main');
 export function sentData(e) {
     e.preventDefault();
     var data = $('.contact_form').serialize();
-    var $fname = $('#fname').val();
-    var $email_one = $('#email').val();
-    var $email_two = $('#confirm_email').val();
-    var $mgs = $('#message').val();
+    const $FNAME = $('#fname').val();
+    const $EMAIL_ONE = $('#email').val();
+    const $EMAIL_TWO = $('#confirm_email').val();
+    const $MSG = $('#message').val();
     const $REG = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*(\.\w{2,})+$/;
 
-    if ($fname === '') {
+    if ($FNAME === '') {
         $('#fname').css({'border-color': 'red', 'border-width': '2px'});
         return;
     }
-    else if ($mgs === '') {
+    else if ($MSG === '') {
         $('#message').css({'border-color': 'red', 'border-width': '2px'});
         return;
     }
-    else if ($email_one !== $email_two || $email_one === '') {
+    else if ($EMAIL_ONE !== $EMAIL_TWO || $EMAIL_ONE === '') {
         $('#email').css({'border-color': 'red', 'border-width': '2px'});
         $('#confirm_email').css({'border-color': 'red', 'border-width': '2px'});
         return;
     }
-    else if ($REG.test($email_two)) {
+    else if ($REG.test($EMAIL_TWO)) {
         $.post('./php/action_page.php', data)
         .done(function() {
-            showModal(`${$fname}, <br/> form was submited successfuly. <br/><br/>
+            showModal(`${$FNAME}, <br/> form was submited successfuly. <br/><br/>
                         We will contact you within 24 h. <br/><br/>
                         Thank you!`);
-                    $fname = '';
-                    $email_one = '';
-                    $email_two = '';
-                    $mgs = '';})
+                    $('#fname').val('');
+                    $('#email').val('');
+                    $('#confirm_email').val('');
+                    $('#message').val('');})
         .fail(function(){
-            showModal(`${$fname}, <br/> something went wrong. <br/><br/>
+            showModal(`${$FNAME}, <br/> something went wrong. <br/><br/>
                     Try again or write directly: <br/>
                     info@starcab.wroclaw.pl`)})
             return;
